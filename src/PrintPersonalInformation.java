45fdca048a44850594a6f39f1c5dd7c1db7c326f
Keila777 <killik1985.ks@gmail.com>
2024-03-04 22:39:30 -0500

    program corrections

---
+++ src/PrintPersonalInformation.java
@@ -0,0 +1,15 @@
+public class PrintPersonalInformation {
+public static void main(String[] argos) {
+    String firstName = "Robert";
+    String lastName = "Allen";
+    String streetAddress = "14151 Chandler Park Dr.";
+    String city = "Kennett, Mo.";
+    String zipcode ="63857";
+    System.out.println("First Name: " + firstName);
+    System.out.println("Last Name: " + lastName);
+    System.out.println("Street Address: " + streetAddress);
+    System.out.println("City: " + city);
+    System.out.println("Zipcode: " + zipcode);
+
+        }
+    }
